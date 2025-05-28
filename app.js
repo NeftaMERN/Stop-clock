e809681f88392ff3d5bde17d670af0dbfd15a815
Neftalem <dneftalem@gmail.com>
2025-05-28 16:38:24 +0300

    start js cod write

---
+++ app.js
@@ -0,0 +1,19 @@
+
+document.getElementById("display");
+
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRanning = false;
+
+function start() {
+
+    if(!isRanning) {
+
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(upload, 10);
+        isRanning = true;
+
+    }
+
+}
